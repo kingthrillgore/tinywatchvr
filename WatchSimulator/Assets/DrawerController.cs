96a0c2882b6094c6b832bff56951e9bd517ad037
jesse-scam <36874551+jesse-scam@users.noreply.github.com>
2020-01-31 23:13:19 -0600

    Drawers and also a morgue

---
+++ WatchSimulator/Assets/DrawerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawerController : MonoBehaviour
+{
+    float distanceToMove = 1f;
+    float moveTime = 1f;
+    bool open = false;
+    bool moving = false;
+
+    public GameObject[] spawnableObjects;
+    GameObject spawnedObject;
+
+
+    void spawnRandom() {
+        spawnedObject = GameObject.Instantiate(spawnableObjects[Random.Range(0, spawnableObjects.Length - 1)], this.transform);
+        spawnedObject.transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+    }
+
+    void handleCollision(Collider col) {
+        Debug.Log("Colliding with " + col.name);
+
+        if (open) {
+            drawerClose();
+        } else {
+            drawerOpen();
+        }
+    }
+
+    void drawerOpen() {
+        if (moveDrawer(transform.forward)) {
+            open = true;
+
+            spawnRandom();
+        }
+    }
+
+    void drawerClose() {
+        if (moveDrawer(-transform.forward)) {
+            open = false;
+
+            Destroy(spawnedObject);
+        }
+    }
+
+    bool moveDrawer(Vector3 direction) {
+        if (moving) {
+            return false;
+        }
+        StartCoroutine(moveDrawerCoroutine(direction));
+        return true;
+    }
+
+    IEnumerator moveDrawerCoroutine(Vector3 direction) {
+        moving = true;
+
+        Vector3 start = transform.position;
+        Vector3 destination = transform.position + (direction * distanceToMove);
+        float t = 0f;
+
+        while (t < 1f) {
+            t += Time.deltaTime / moveTime;
+            transform.position = Vector3.Lerp(start, destination, t);
+            yield return new WaitForEndOfFrame();
+        }
+
+        moving = false;
+        yield return null;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        handleCollision(collision.collider);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        handleCollision(other);
+    }
+}
