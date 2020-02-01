27430058ccd179f2baa9e749ecb8acc7578e6658
jesse-scam <36874551+jesse-scam@users.noreply.github.com>
2020-02-01 00:44:38 -0600

    Start of zoom stuff

---
+++ WatchSimulator/Assets/ZoomController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomController : MonoBehaviour
+{
+
+    public Transform cameraParent;
+    public float multiplier = 2f;
+    bool scaled = false;
+
+    void handleCollision(Collider col) {
+        Debug.Log("Colliding with " + col.name);
+
+        toggleScale();
+    }
+
+    void toggleScale() {
+        if (scaled) {
+            cameraParent.localScale /= multiplier;
+        } else {
+            cameraParent.localScale *= multiplier;
+        }
+
+        scaled = !scaled;
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        handleCollision(collision.collider);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        handleCollision(other);
+    }
+}
