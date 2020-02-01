8aedb1a77ff77f1f153d6c78bd010a64f117429a
jesse-scam <36874551+jesse-scam@users.noreply.github.com>
2020-02-01 14:44:53 -0600

    Hand animations

---
+++ WatchSimulator/Assets/HandAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class HandAnimator : MonoBehaviour
+{
+    public VRTK.VRTK_InteractGrab grab;
+    public Animator animator;
+    string fistBool = "MakingFist";
+    string daintBool = "HoldingDaintily";
+
+    private void OnEnable() {
+        grab.ControllerGrabInteractableObject += handleGrab;
+        grab.ControllerUngrabInteractableObject += handleUngrab;
+    }
+
+    private void OnDisable() {
+        grab.ControllerGrabInteractableObject -= handleGrab;
+        grab.ControllerUngrabInteractableObject += handleUngrab;
+    }
+
+    void handleGrab(object sender, ObjectInteractEventArgs e) {
+        animator.SetBool(daintBool, true);
+    }
+
+    void handleUngrab(object sender, ObjectInteractEventArgs e) {
+        animator.SetBool(daintBool, false);
+    }
+}
