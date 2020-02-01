1f9ff50e316d6599804f19be57fd66973ec250e5
Colby DeHart <colbydehart@gmail.com>
2020-01-31 23:58:49 -0600

    grabbing parts and detecting collisions!

---
+++ WatchSimulator/Assets/Scripts/WatchPart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchPart : MonoBehaviour
+{
+  public bool valid;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+
+  }
+}
---
+++ WatchSimulator/Assets/Scripts/WatchSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchSlot : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+  }
+
+  private void onCollision(Collider collider)
+  {
+    if (collider.TryGetComponent<WatchPart>(out WatchPart part) && part.valid)
+    {
+      // TODO: actually do something? snap the part to the slot.
+      Debug.Log("WE HAVE COLLIDED WITH A GOOD PART HERE BABY!");
+    }
+  }
+
+  private void OnCollisionEnter(Collision collision)
+  {
+    onCollision(collision.collider);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    onCollision(other);
+  }
+}
