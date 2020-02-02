e826b701c38e8a00304f0830a45d4c02035db293
Colby DeHart <colbydehart@gmail.com>
2020-02-02 12:51:53 -0600

    added phone silencing click

--- WatchSimulator/Assets/AlertToggle.cs
+++ WatchSimulator/Assets/AlertToggle.cs
@@ -6,15 +6,17 @@ using TMPro;
 public class AlertToggle : MonoBehaviour
 {
   public AudioSource source;
+  public AudioSource clickSource;
   public GameObject canvas;
-    public TextMeshProUGUI randomText;
+  public TextMeshProUGUI randomText;
   float alertDelay = 10f;
   public bool scatterOnAwake = false;
   public RigidbodyScatterer scatter;
   public GameObject blankScreen;
   public Transform explosionPos;
+  bool silenced = true;
 
-    string[] alerts = { "SUN STILL EXISTS", "EVERYTHING IS FINE", "NOT FLOODING", "KINDA WARM", "NO WIND", "SMELLS NICE" };
+  string[] alerts = { "SUN STILL EXISTS", "EVERYTHING IS FINE", "NOT FLOODING", "KINDA WARM", "NO WIND", "SMELLS NICE" };
 
   private void OnEnable()
   {
@@ -39,12 +41,14 @@ public class AlertToggle : MonoBehaviour
   IEnumerator snooze()
   {
     yield return new WaitForSeconds(90f * Random.value);
+    silenced = false;
     StartCoroutine(alertCoroutine());
   }
 
 
   IEnumerator alertCoroutine()
   {
+    silenced = false;
     while (true)
     {
       source.Stop();
@@ -54,25 +58,33 @@ public class AlertToggle : MonoBehaviour
       source.Play();
       if (scatter != null)
       {
-        if (explosionPos != null) {
-            scatter.scatterAtPoint(explosionPos);
-        } else {
-            scatter.scatterAtPoint(this.transform);
+        if (explosionPos != null)
+        {
+          scatter.scatterAtPoint(explosionPos);
+        }
+        else
+        {
+          scatter.scatterAtPoint(this.transform);
         }
       }
 
-        if (!scatterOnAwake) {
-            yield return new WaitForSeconds(alertDelay);
-        } else {
-            yield break;
-        }
+      if (!scatterOnAwake)
+      {
+        yield return new WaitForSeconds(alertDelay);
+      }
+      else
+      {
+        yield break;
+      }
     }
   }
 
   void onCollision(Collider collider)
   {
-    if (collider.tag == "Hand" || (collider.transform.parent != null && collider.transform.parent.tag == "Hand"))
+    if (collider.tag == "Hand" || (collider.transform.parent != null && collider.transform.parent.tag == "Hand" && !silenced))
     {
+      silenced = true;
+      clickSource.Play();
       StopAllCoroutines();
       source.Stop();
       blankScreen.SetActive(true);
